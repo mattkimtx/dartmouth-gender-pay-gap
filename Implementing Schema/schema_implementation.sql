9d25bde950c5961341fd5950cfe8b7ea2bb2ea71
Matthew Kim <matthewkimtx@gmail.com>
2023-08-14 23:37:28 -0400

    8/14/23

---
+++ Implementing Schema/schema_implementation.sql
@@ -0,0 +1,37 @@
+use gender_pay_gap;
+
+create table Salary (
+	sl_id int,
+    base_pay int,
+    bonus_pay int,
+    primary key (sl_id)
+    );
+
+create table Qualities (
+	ql_id int,
+    education varchar(45),
+    perf_score int,
+    seniority int,
+	primary key (ql_id)
+    );
+
+CREATE TABLE Employee (
+	ep_id int,
+    sl_id int,
+    ql_id int,
+    job_name varchar(45),
+    dept_name varchar(45),
+    gender varchar(45),
+    age int,
+	primary key(ep_id),
+	CONSTRAINT Employee_fk_Salary
+		FOREIGN KEY (sl_id)
+        REFERENCES Salary(sl_id),
+	CONSTRAINT Employee_fk_Qualities
+		FOREIGN KEY (ql_id)
+        REFERENCES Qualities(ql_id)
+    );
+    
+SELECT * from Salary;
+SELECT * from Qualities;
+SELECT * from Employee;
