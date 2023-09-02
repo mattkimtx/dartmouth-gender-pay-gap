5df64c65e332eb7e38e1c25433bd24df3a10d36b
Matthew Kim <matthewkimtx@gmail.com>
2023-09-02 14:52:24 +0800

    forgot about question queries update

--- Enhancing Database/WebInterfaceFiles/Questionqueries.sql
+++ Enhancing Database/WebInterfaceFiles/Questionqueries.sql
@@ -5,6 +5,14 @@ use gender_pay_gap;
 
 SELECT AVG(Base + Bonus) AS Salary from Employees where Job = "Drive";
 
-SELECT Education, Gender, (Base+Bonus) As Salary
+SELECT Education, Gender, Round(AVG(Base+Bonus),2) As Salary
 from Employees 
-group by Education;
+group by Education, Gender;
+
+SELECT Seniority, Gender, Round(AVG(Base+Bonus),2) As Salary
+from Employees 
+group by Seniority, Gender;
+
+SELECT Performance, Gender, Round(AVG(Base+Bonus),2) As Salary
+from Employees 
+group by Performance, Gender;
